aa9d1eb3c550225feb26ae6dfc5e6e286590b311
Andre Anggono <andre.anggono@hotmail.com>
2020-12-09 02:05:38 +1100

    Added comments. Completed Strings homework

--- Andre Anggono/week_01/day_02/strings/js/strings.js
+++ Andre Anggono/week_01/day_02/strings/js/strings.js
@@ -12,7 +12,10 @@ const drEvil = function (amount) {
 
 // MixUp Function
 const mixUp = function (s1, s2) {
+	// Take the first two characters from s2 args, concatenate with s1 args starting from index 2 of the string.
 	const firstMixedWord = s2.substr(0, 2) + s1.substr(2);
+
+	// Take the first two characters from s1 args, concatenate with s2 args starting from index 2 of the string.
 	const secondMixedWord = s1.substr(0, 2) + s2.substr(2);
 	return `${firstMixedWord} ${secondMixedWord}`;
 };
@@ -42,10 +45,13 @@ const fixStart = function (string) {
 const verbing = function (verb) {
 	let result = verb;
 
+	// To ensure argument entered has atleast 3 characters. Otherwise return the argument unchanged.
 	if (verb.length >= 3) {
+		// If verb argument ends in "ing", add "ly" to it
 		if (verb.substr(-3) === "ing") {
 			result += "ly";
 		} else {
+			// Otherwise add "ing"
 			result += "ing";
 		}
 	}
@@ -55,10 +61,15 @@ const verbing = function (verb) {
 
 // Not Bad Function
 const notBad = function (string) {
+	// Find the index where the word "not" starts
 	const indexOfNot = string.indexOf("not");
+
+	// Find the index where the word "bad" starts
 	const indexOfBad = string.indexOf("bad");
 	let result = string;
 
+	// -1 value suggests that the word searched for in the string above is not present
+	// This statement makes sure that they both exists and the order of "not" should come before "bad"
 	if (indexOfNot !== -1 && indexOfBad !== -1 && indexOfBad > indexOfNot) {
 		result = `${string.substr(0, indexOfNot)}good${string.substr(indexOfBad + 3)}`;
 	}
