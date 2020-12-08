49ca471446d671e3cdb184d8de11e9719fc9460c
Andre Anggono <andre.anggono@hotmail.com>
2020-12-08 22:24:57 +1100

    Completed Part 1 of homework

--- Andre Anggono/week_01/day_02/calculator/js/calc.js
+++ Andre Anggono/week_01/day_02/calculator/js/calc.js
@@ -2,5 +2,31 @@
 // Part 1
 
 // Function to square a number
+const squareNumber = function (num) {
+    const result = num ** 2;
+    console.log(`The result of squaring the number ${num} is ${result}.`);
+    return result;
+};
+
+// Function to find the half of a number
+const halfNumber = function (num) {
+    const result = num / 2;
+    console.log (`Half of ${num} is ${result}.`);
+    return result;
+};
+
+// Function to find the percentage of the first value to the second value
+const percentOf = function (num1, num2) {
+    const result = num1 / num2 * 100;
+    console.log(`${num1} is ${result}% of ${num2}.`);
+    return result;
+};
+
+// Function to calculate the area of a circle
+const areaOfCircle = function (radius) {
+    const result = (Math.PI * (radius ** 2)).toFixed(2); // Formula: Pi r^2; .toFixed(2): round up to 2 decimal points;
+    console.log(`The area for a circle with radius ${radius} is ${result}`);
+    return result;
+};
 
 // Part 2
