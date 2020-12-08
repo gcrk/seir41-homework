16a254bde5b5ebfc0bf6a97bbc4c675006bd2a6a
Andre Anggono <andre.anggono@hotmail.com>
2020-12-08 23:06:34 +1100

    Added new DrEvil Function

--- Andre Anggono/week_01/day_02/strings/js/strings.js
+++ Andre Anggono/week_01/day_02/strings/js/strings.js
@@ -1,2 +1,11 @@
 // Strings
+// DrEvil Function
+const drEvil = function (amount) {
+    let result = `${amount} dollars`;
 
+    if (amount === 1000000) {
+        result += " (pinky)";
+    }
+
+    return result;
+};
