7745ed8ced3d2dce1cf13a9e87a3a816e92be542
Danielle <danisteele1984@gmail.com>
2020-12-09 18:34:46 +1100

    First homework commit

---
+++ Danielle Steele/week1/day3/homework.js
@@ -0,0 +1,24 @@
+//Array and Functions Bonus Material
+
+//Define a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. You'll have to remember your pre-work, or do some googling to figure this out.
+
+//Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+
+//Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+
+//Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
+
+
+//Homework: The Word Guesser
+
+//You'll create a simple word guessing game where the user gets infinite tries to guess the word (like Hangman without the hangman, or like Wheel //of Fortune without the wheel and fortune).
+//
+//Create two global arrays: one to hold the letters of the word (e.g. 'F', 'O', 'X'), and one to hold the current guessed letters (e.g. it would //start with '_', '_', '_' and end with 'F', 'O', 'X').
+//Write a function called guessLetter that will:
+//Take one argument, the guessed letter.
+//Iterate through the word letters and see if the guessed letter is in there.
+//If the guessed letter matches a word letter, changed the guessed letters array to reflect that.
+//When it's done iterating, it should log the current guessed letters ('F__') and congratulate the user if they found a new letter.
+//It should also figure out if there are any more letters that need to be guessed, and if not, it should congratulate the user for winning the //game.
+//Pretend you don't know the word, and call guessLetter multiple times with various letters to check that your program works.
+
