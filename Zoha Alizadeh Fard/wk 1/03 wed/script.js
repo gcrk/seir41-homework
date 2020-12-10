0af00ccedd907066df698f493c562dfbb573d049
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2020-12-10 15:58:02 +1100

    INT_COMMIT

--- Zoha Alizadeh Fard/wk 1/03 wed/script.js
+++ Zoha Alizadeh Fard/wk 1/03 wed/script.js
@@ -1,51 +1,117 @@
-// # Warmup - Raindrops
+// # Array and Functions Bonus Material
 
-// Write a Javascript program that will take a number (eg 28 or 1755 or 9, etc) and output the following:
+// 1. Define a function `maxOfTwoNumbers` that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript. You'll have to remember your pre-work, or do some googling to figure this out.
 
-// If the number contains 3 as a factor, output 'Pling'.
-// If the number contains 5 as a factor, output 'Plang'.
-// If the number contains 7 as a factor, output 'Plong'.
-// If the number does not contain 3, 5, or 7 as a factor, output the number as a string.
-
-
-
-const factors = function(n) {
- var num_factors = [], i;
- 
- for (i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
-  if (n % i === 0)
-  {
-   num_factors.push(i);
-   if (n / i !== i)
-    num_factors.push(n / i);
+function maxOfTwoNumbers(a, b) {
+  let greater;
+  if (a < b) {
+  greater = b
+  } else {
+    greater = a
   }
- num_factors.sort(function(x, y)
-   {
-     return x - y;});  // numeric sort
-     return num_factors;
-    }
+    return greater;
+  };
+  console.log(maxOfTwoNumbers(3, 66));
+
+// 2. Define a function `maxOfThree` that takes three numbers as arguments and returns the largest of them.
+
+function maxOfThree(n1, n2, n3) {
+  let greatest;
+if (n1 > n2 && n1 > n3) {
+  greatest = n1;
+} else if (n2 > n1 && n2 > n3) {
+  greatest = n2;
+} else {
+  greatest = n3;
+};
+return greatest;
+};
+
+console.log(maxOfThree(3, 5, 7))
+console.log(maxOfThree(5, 7, 3))
+console.log(maxOfThree(7, 5, 3))
+
+// 3. Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+
+//we want a function that would return false if there was no vowel and return true if the arguemnt is a vowel
+// const letter = letter.toLowerCase();
 
 
-console.log(factors(15));  // [1,3,5,15] 
-// console.log(factors(16));  // [1,2,4,8,16] 
-// console.log(factors(17));  // [1,17]
 
-const raindrops = function () {
-    if (factors.includes(3)) {
-        return "Pling";
-    };
-    
+function vowelFinder(letter) {
+  let l = letter.toLowerCase();
+  if (l === 'a' || l === 'e' || l === 'i' || l === 'o' || l === 'u' || l === 'y' || l === 'w'){
+    return true
+  } else {
+    return false
+  }
+ 
+};
+
+// 4. Define a function `sumArray` and a function `multiplyArray` that sums and multiplies (respectively) all the numbers in an array of numbers. For example, `sumArray([1,2,3,4])` should return 10, and `multiplyArray([1,2,3,4])` should return 24.
+
+
+function sumArray([n1, n2, n3, n4]) {
+  return n1 + n2 + n3 + n4;
+}
+console.log(sumArray([2, 4, 5, 6]))
+
+function multiplyArray([n1, n2, n3, n4]) {
+  return n1 * n2 * n3 * n4;
+}
+console.log(multiplyArray([2, 4, 5, 6]))
+
+
+// ## Bonus
+
+// 5. Define a function `reverseString` that computes the reversal of a string. For example, reverseString("jag testar") should return the string "ratset gaj".
+// 6. Write a function `findLongestWord` that takes an array of words and returns the length of the longest one.
+// 7. Write a function `filterLongWords` that takes an array of words and an number `i` and returns the array of words that are longer than i.
+
+
+
+
+
+// # Homework: The Word Guesser
+
+// You'll create a simple word guessing game where the user gets infinite tries to guess the word (like Hangman without the hangman, or like Wheel of Fortune without the wheel and fortune).
+
+// - Create two global arrays: one to hold the letters of the word (e.g. 'F', 'O', 'X'), and one to hold the current guessed letters (e.g. it would start with '\_', '\_', '\_' and end with 'F', 'O', 'X').
+// - Write a function called guessLetter that will:
+// - Take one argument, the guessed letter.
+// - Iterate through the word letters and see if the guessed letter is in there.
+// - If the guessed letter matches a word letter, changed the guessed letters array to reflect that.
+// - When it's done iterating, it should log the current guessed letters ('F__')
+// and congratulate the user if they found a new letter.
+// - It should also figure out if there are any more letters that need to be guessed,
+// and if not, it should congratulate the user for winning the game.
+// - Pretend you don't know the word, and call guessLetter multiple times with various letters to check that your program works.
+
+let wordLetters = ['b', 'e', 'n', 'c', 'h'];
+
+let guessedLetters = ['\_', '\_', '\_', '\_', '\_'];
+
+
+function theWordGuesser(guessedLetter) {
+  for (let i = 0; i <= wordLetters.length; i++) {
+    return wordLetter = wordLetters[i]
+  };
+
+  if(guessedLetter === wordLetter){
+    guessedLetters[i] = wordLetter;
+  };
+
+console.log(guessedLetters)
 
 };
 
-console.log(raindrops);
 
+// ## Bonus: Make it more like Wheel of Fortune:
+// - Start with a reward amount of $0
+// - Every time a letter is guessed, generate a random amount and reward the user if they found a letter (multiplying the reward if multiple letters found), otherwise subtract from their reward.
+// - When they guess the word, log their final reward amount.
 
-// ### Examples
-
-// 28 has 7 as a factor.
-// In raindrop-speak, this would be a simple "Plong".
-// 1755 has 3 and 5 as factors.
-// In raindrop-speak, this would be a "PlingPlang".
-// 34 has neither 3, 5 nor 7 as a factor.
-// Raindrop-speak doesn't know what to make of that, so it just goes with the straightforward "34".
+// ## Bonus: Make it like Hangman:
+// - Keep track of all the guessed letters (right and wrong) and only let the user guess a letter once. If they guess a letter twice, do nothing.
+// - Keep track of the state of the hangman as a number (starting at 0), and subtract or add to that number every time they make a wrong guess.
+// - Once the number reaches 6 (a reasonable number of body parts for a hangman), inform the user that they lost and show a hangman on the log.
