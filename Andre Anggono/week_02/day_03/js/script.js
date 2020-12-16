9c95af4fe6e6af1429d752f04668f95b61d49251
Andre Anggono <andre.anggono@hotmail.com>
2020-12-17 10:04:35 +1100

    Added cat dancing and background disco

--- Andre Anggono/week_02/day_03/js/script.js
+++ Andre Anggono/week_02/day_03/js/script.js
@@ -15,30 +15,77 @@ const catWalkRight = function (data, moves, screen) {
 	let oldLeft = parseInt(data.style.left);
 	data.style.right = "";
 	if (oldLeft > screen) {
-		// data.removeAttribute("style");
-		// console.log(data.left);
-		// document.querySelector("#walking-cat-right").style.left = "";
 		data.style.right = "0px";
 		data.id = "walking-cat-left";
 	}
 
-	const newLeft = oldLeft + moves;
-	data.style.left = newLeft + "px";
+	if (oldLeft < screen / 2 || oldLeft > screen / 2 + 2) {
+		const newLeft = oldLeft + moves;
+		data.style.left = newLeft + "px";
+	} else {
+		data.style.display = "none";
+		clearInterval(walkTimer);
+		dancingCat(oldLeft, true);
+		setTimeout(function () {
+			stopDancingCat(data, moves, oldLeft, true);
+		}, 10000);
+	}
 };
 
 const catWalkLeft = function (data, moves, screen) {
 	let oldRight = parseInt(data.style.right);
 	data.style.left = "";
 	if (oldRight > screen) {
-		// data.removeAttribute("style");
-		// document.querySelector("#walking-cat-left").style.right = "";
 		data.style.left = "0px";
 		data.id = "walking-cat-right";
-		console.log(document.querySelector("#walking-cat-right"));
 	}
 
-	const newRight = oldRight + moves;
-	data.style.right = newRight + "px";
+	if (oldRight < screen / 2 || oldRight > screen / 2 + 2) {
+		const newRight = oldRight + moves;
+		data.style.right = newRight + "px";
+	} else {
+		data.style.display = "none";
+		clearInterval(walkTimer);
+		dancingCat(oldRight, false);
+		setTimeout(function () {
+			stopDancingCat(data, moves, oldRight, false);
+		}, 10000);
+	}
 };
 
-const walkTimer = setInterval(catWalk, 40);
+const dancingCat = function (position, isLeft) {
+	const dancingCat = document.querySelector("#dancing-cat");
+
+	dancingCat.style = "";
+
+	if (isLeft) {
+		dancingCat.style.left = position + "px";
+	} else {
+		dancingCat.style.right = position + "px";
+	}
+
+	document.body.style.backgroundImage = 'url("img/dance-floor2.jpg")';
+	document.body.style.backgroundSize = "cover";
+	dancingCat.style.display = "inline";
+};
+
+const stopDancingCat = function (data, moves, directionValue, isLeft) {
+	const dancingCat = document.querySelector("#dancing-cat");
+	dancingCat.style.display = "none";
+
+	data.style.display = "inline";
+
+	// cat going right
+	if (isLeft) {
+		const newLeft = directionValue + moves;
+		data.style.left = newLeft + "px";
+	} else {
+		const newRight = directionValue + moves;
+		data.style.right = newRight + "px";
+	}
+
+	document.body.style = "";
+	walkTimer = setInterval(catWalk, 40);
+};
+
+let walkTimer = setInterval(catWalk, 40);
