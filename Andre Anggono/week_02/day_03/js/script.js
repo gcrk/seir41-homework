6dbdc7a84896eb82256d589b84763fa3203f7a03
Andre Anggono <andre.anggono@hotmail.com>
2020-12-17 00:13:46 +1100

    made cat walk back and forth using different ID tags

--- Andre Anggono/week_02/day_03/js/script.js
+++ Andre Anggono/week_02/day_03/js/script.js
@@ -1,17 +1,44 @@
-const cWalk = document.querySelector("#walking-cat");
-
 const catWalk = function () {
+	const cWalkRight = document.querySelector("#walking-cat-right");
+	const cWalkLeft = document.querySelector("#walking-cat-left");
+	const moves = 6;
 	const edgeOfScreen = window.innerWidth - 296;
-	let oldLeft = parseInt(cWalk.style.left);
 
-	if (oldLeft > edgeOfScreen) {
-		console.log(`this is the end cWalk left is ${cWalk.style.left}`);
-		oldLeft = 0;
+	if (cWalkRight) {
+		catWalkRight(cWalkRight, moves, edgeOfScreen);
+	} else {
+		catWalkLeft(cWalkLeft, moves, edgeOfScreen);
 	}
-
-	const newLeft = oldLeft + 6;
-	cWalk.style.left = newLeft + "px";
-	// cWalk.style.left = edgeOfScreen + "px";
 };
 
-// const walkTimer = setInterval(catWalk, 40);
+const catWalkRight = function (data, moves, screen) {
+	let oldLeft = parseInt(data.style.left);
+	data.style.right = "";
+	if (oldLeft > screen) {
+		// data.removeAttribute("style");
+		// console.log(data.left);
+		// document.querySelector("#walking-cat-right").style.left = "";
+		data.style.right = "0px";
+		data.id = "walking-cat-left";
+	}
+
+	const newLeft = oldLeft + moves;
+	data.style.left = newLeft + "px";
+};
+
+const catWalkLeft = function (data, moves, screen) {
+	let oldRight = parseInt(data.style.right);
+	data.style.left = "";
+	if (oldRight > screen) {
+		// data.removeAttribute("style");
+		// document.querySelector("#walking-cat-left").style.right = "";
+		data.style.left = "0px";
+		data.id = "walking-cat-right";
+		console.log(document.querySelector("#walking-cat-right"));
+	}
+
+	const newRight = oldRight + moves;
+	data.style.right = newRight + "px";
+};
+
+const walkTimer = setInterval(catWalk, 40);
