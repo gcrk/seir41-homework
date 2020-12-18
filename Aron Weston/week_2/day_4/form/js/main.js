4f87f23d94319e083dd7eef59cce5fd55e86fd7b
aronweston <aron@firststepdigital.com.au>
2020-12-18 12:47:21 +1100

    form changes

--- Aron Weston/week_2/day_4/form/js/main.js
+++ Aron Weston/week_2/day_4/form/js/main.js
@@ -1,5 +1,8 @@
 $(function () {
 
+    //Really simple get request using AJAX. I love how simple this instead of using the XHR object in vanilla JS. But the fetch API is still less code and a quicker AJAX tech. 
+
+    //But still fun to learn the way they had to do it back in the day. 
     $('#get-cat').on('click', function () {
 
         $.get("https://api.thecatapi.com/v1/images/search?api_key=${config.key}&mime_types=gif", function (data, status) {
