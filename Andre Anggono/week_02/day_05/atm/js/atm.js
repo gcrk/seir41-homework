d9f19f097b500cbcda138c10b4efee8352e41467
Andre Anggono <andre.anggono@hotmail.com>
2020-12-19 01:11:46 +1100

    Added bank mehods

--- Andre Anggono/week_02/day_05/atm/js/atm.js
+++ Andre Anggono/week_02/day_05/atm/js/atm.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {});
 
 const bank = {
-	checking: 0,
-	savings: 0,
-	deposit: function (account, amount) {
-		bank.account += amount;
+	accounts: {
+		checking: 0,
+		savings: 0
 	},
-	withdraw: function (account, amount) {
-		if (bank.account >= amount) {
-			bank.account -= amount;
+	deposit: function (accType, amount) {
+		this.accounts[accType] += amount;
+	},
+	withdraw: function (accType, amount) {
+		let overdraftAcc;
+		const accBalance = this.accounts[accType];
+		const totalBalance = this.accounts.checking + this.accounts.savings;
+
+		if (accType === "checking") overdraftAcc = "savings";
+		else overdraftAcc = "checking";
+
+		if (accBalance >= amount) this.accounts[accType] -= amount;
+		else if (totalBalance >= amount) {
+			this.accounts[accType] -= accBalance;
+			this.accounts[overdraftAcc] -= amount - accBalance;
 		}
 	}
 };
