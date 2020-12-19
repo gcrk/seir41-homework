cb5893051680c1a3c8fe53b16d968a1014734eda
Andre Anggono <andre.anggono@hotmail.com>
2020-12-19 16:58:49 +1100

    completed ATM Homework

--- Andre Anggono/week_02/day_05/atm/js/atm.js
+++ Andre Anggono/week_02/day_05/atm/js/atm.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-	updateBalance(); // Redundant - incase HTML initial value is broken or changed. Just making sure the correct balance is displayed on load
-	$("#checking-deposit").on("click", bank.deposit("checking", Number($("#checking-amount").val()))); // Checking - Deposit
-	$("#checking-withdraw").on("click", bank.withdraw("checking", Number($("#checking-amount").val()))); // Checking - Withdraw
-	$("#savings-deposit").on("click", bank.deposit("savings", Number($("#savings-amount").val()))); // Savings - Deposit
-	$("#savings-withdraw").on("click", bank.withdraw("savings", Number($("#savings-amount").val()))); // Savings - Withdraw
+	bank.updateBalance(); // Making sure the correct balance is displayed on load.
+
+	// Event Listener on all 4 buttons on click.
+	$("#checking-deposit, #checking-withdraw, #savings-deposit, #savings-withdraw").on("click", function () {
+		const transaction = this.value.toLowerCase(); // Grabs the value attribute of the button that triggered the event to know if it's a withdrawal or deposit.
+		const acc = this.id.replace(`-${transaction}`, ""); // Grabs the Account type that the user is Depositing to / Withdrawing from.
+		bank[transaction](acc, Math.abs($(`#${acc}-amount`).val())); // Invoke the Bank Object Method below based on the Account and Transaction type above.
+	});
 });
 
 const bank = {
@@ -11,30 +14,35 @@ const bank = {
 		checking: 0,
 		savings: 0
 	},
-	deposit: function (accType, amount) {
+	deposit(accType, amount) {
 		this.accounts[accType] += amount;
-		updateBalance();
+		this.updateBalance();
 	},
-	withdraw: function (accType, amount) {
+	withdraw(accType, amount) {
 		let overdraftAcc;
 		const accBalance = this.accounts[accType];
 		const totalBalance = this.accounts.checking + this.accounts.savings;
 
-		if (accType === "checking") {
-			overdraftAcc = "savings";
-		} else overdraftAcc = "checking";
+		accType === "checking" ? (overdraftAcc = "savings") : (overdraftAcc = "checking"); // Determine what the overdraft account is (should be the opposite of the targeted account).
 
 		if (accBalance >= amount) {
-			this.accounts[accType] -= amount;
+			this.accounts[accType] -= amount; // Allow normal withdrawal if there is sufficient balance.
 		} else if (totalBalance >= amount) {
-			this.accounts[accType] -= accBalance;
-			this.accounts[overdraftAcc] -= amount - accBalance;
+			this.accounts[accType] -= accBalance; // Insufficient balance on current account but total balance on both accounts are sufficient, withdraw everything from current account.
+			this.accounts[overdraftAcc] -= amount - accBalance; // Access overdraft account for the rest of the amount being withdrawn.
 		}
-		updateBalance();
-	}
-};
+		this.updateBalance();
+	},
+	updateBalance() {
+		const $checkingBalance = $("#checking-balance"); // Storing jQuery object for div id #checking-balance.
+		const $savingsBalance = $("#savings-balance"); // Storing jQuery object for div id #savings-balance.
 
-const updateBalance = () => {
-	$("#checking-balance").text(`$${bank.accounts.checking}`);
-	$("#savings-balance").text(`$${bank.accounts.savings}`);
+		// Changing div balance background to Red / Gray by adding / removing .zero class. CSS changes the colour. $0 === Red , Balance in account === Gray.
+		bank.accounts.checking === 0 ? $checkingBalance.addClass("zero") : $checkingBalance.removeClass("zero"); // For Checking balance div.
+		bank.accounts.savings === 0 ? $savingsBalance.addClass("zero") : $savingsBalance.removeClass("zero"); // For Savings balance div.
+
+		// Update HTML div balance text to reflect the balance of each accounts.
+		$checkingBalance.text(`$${bank.accounts.checking}`);
+		$savingsBalance.text(`$${bank.accounts.savings}`);
+	}
 };
