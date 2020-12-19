c4ba3ec5d6818d1b2c3172de329dcd3a21a3a1c3
Andre Anggono <andre.anggono@hotmail.com>
2020-12-19 12:43:17 +1100

    Added GUI functionality

--- Andre Anggono/week_02/day_05/atm/js/atm.js
+++ Andre Anggono/week_02/day_05/atm/js/atm.js
@@ -1,4 +1,10 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	updateBalance(); // Redundant - incase HTML initial value is broken or changed. Just making sure the correct balance is displayed on load
+	$("#checking-deposit").on("click", bank.deposit("checking", Number($("#checking-amount").val()))); // Checking - Deposit
+	$("#checking-withdraw").on("click", bank.withdraw("checking", Number($("#checking-amount").val()))); // Checking - Withdraw
+	$("#savings-deposit").on("click", bank.deposit("savings", Number($("#savings-amount").val()))); // Savings - Deposit
+	$("#savings-withdraw").on("click", bank.withdraw("savings", Number($("#savings-amount").val()))); // Savings - Withdraw
+});
 
 const bank = {
 	accounts: {
@@ -7,19 +13,28 @@ const bank = {
 	},
 	deposit: function (accType, amount) {
 		this.accounts[accType] += amount;
+		updateBalance();
 	},
 	withdraw: function (accType, amount) {
 		let overdraftAcc;
 		const accBalance = this.accounts[accType];
 		const totalBalance = this.accounts.checking + this.accounts.savings;
 
-		if (accType === "checking") overdraftAcc = "savings";
-		else overdraftAcc = "checking";
+		if (accType === "checking") {
+			overdraftAcc = "savings";
+		} else overdraftAcc = "checking";
 
-		if (accBalance >= amount) this.accounts[accType] -= amount;
-		else if (totalBalance >= amount) {
+		if (accBalance >= amount) {
+			this.accounts[accType] -= amount;
+		} else if (totalBalance >= amount) {
 			this.accounts[accType] -= accBalance;
 			this.accounts[overdraftAcc] -= amount - accBalance;
 		}
+		updateBalance();
 	}
+};
+
+const updateBalance = () => {
+	$("#checking-balance").text(`$${bank.accounts.checking}`);
+	$("#savings-balance").text(`$${bank.accounts.savings}`);
 };
