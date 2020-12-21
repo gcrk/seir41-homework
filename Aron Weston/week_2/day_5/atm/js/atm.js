5652e98e53c224f8acee5dde8aab59f915a9cadd
aronweston <aron@firststepdigital.com.au>
2020-12-21 16:42:25 +1100

    ATM: Checking deposit/withdraw functional, changed last commit by pushing the negative value to array on withdrawl - much less work. Slight refactor by putting everything in their respective click event.

--- Aron Weston/week_2/day_5/atm/js/atm.js
+++ Aron Weston/week_2/day_5/atm/js/atm.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     const output = (account, total) => {
         console.log(`${account}`, total);
         $('#checking-balance').text(`$${total}`);
-        $('#checking-amount').val('');
+
+        console.log(checkBalance);
     }
 
     const msg = (msg, element, zero) => {
@@ -20,52 +21,104 @@ $(document).ready(function () {
     const clearMsg = () => {
         $('#alert').remove();
         $('#checking').removeClass('zero');
-        $('#checking-amount').val('');
+
     }
 
-    const checkBalance = [];
-
-    const depositCheck = () => {
-        const checkInput = parseInt($('#checking-amount').val())
-
-        if (isNaN(checkInput)) {
-            msg('Insert more than 0', '#checking', true);
-        } else if (checkInput > 0) {
-            $('#checking').removeClass('zero');
-            checkBalance.push(checkInput);
-            let depositTotal = 0;
-            $.each(checkBalance, function () {
-                depositTotal += parseInt(this);
-            })
-            output('DEPOSIT:', depositTotal);
-        }
-    }
-
-
-    const withdrawCheck = () => {
-
-        const checkInput = parseInt($('#checking-amount').val());
-
+    const loop = () => {
         let depositTotal = 0;
         $.each(checkBalance, function () {
             depositTotal += parseInt(this);
         })
-
-        let withdrawTotal = depositTotal - checkInput;
-
-        if (withdrawTotal < 0) {
-            msg("You can't withdraw more than you have", '#checking', true);
-        } else if (withdrawTotal === 0) {
-            msg("You're out of money!", '#checking', true);
-            output('WITHDRAW:', withdrawTotal)
-        } else {
-            output('WITHDRAW:', withdrawTotal)
-        }
+        return depositTotal;
     }
 
-    //CE's
-    $('#checking-deposit').on('click', depositCheck);
-    $('#checking-withdraw').on('click', withdrawCheck);
+
+    //CHECKING 
+
+    const checkBalance = [];
+    $('#checking-deposit').on('click', () => {
+        const checkInput = parseInt($('#checking-amount').val())
+
+        if (isNaN(checkInput) || checkInput <= 0) {
+            msg('Insert more than 0', '#checking', true);
+        } else {
+            checkBalance.push(checkInput);
+            let depositTotal = loop();
+            output('DEPOSIT:', depositTotal);
+        }
+
+    });
+
+
+    //Checking - Withdraw 
+    $('#checking-withdraw').on('click', () => {
+
+        const checkInput = parseInt($('#checking-amount').val());
+
+        let depositTotal = loop();
+
+        const withdrawTotal = depositTotal - checkInput;
+        const debit = withdrawTotal - depositTotal;
+
+        if (isNaN(checkInput)) {
+            msg('You have no more money', '#checking', true);
+        } else {
+            if (withdrawTotal < 0) {
+                msg("You can't withdraw more than you have!", '#checking', true);
+            } else if (withdrawTotal === 0) {
+                msg("You're out of money!", '#checking', true);
+                checkBalance.push(debit);
+                output('WITHDRAW:', withdrawTotal)
+            } else {
+                checkBalance.push(debit);
+                output('WITHDRAW:', withdrawTotal)
+            }
+        }
+
+    });
+
+
+    $('#checking-deposit').on('click', () => {
+        const checkInput = parseInt($('#checking-amount').val())
+
+        if (isNaN(checkInput) || checkInput <= 0) {
+            msg('Insert more than 0', '#checking', true);
+        } else {
+            checkBalance.push(checkInput);
+            let depositTotal = loop();
+            output('DEPOSIT:', depositTotal);
+        }
+
+    });
+
+
+    //Checking - Withdraw 
+    $('#checking-withdraw').on('click', () => {
+
+        const checkInput = parseInt($('#checking-amount').val());
+
+        let depositTotal = loop();
+
+        const withdrawTotal = depositTotal - checkInput;
+        const debit = withdrawTotal - depositTotal;
+
+        if (isNaN(checkInput)) {
+            msg('You have no more money', '#checking', true);
+        } else {
+            if (withdrawTotal < 0) {
+                msg("You can't withdraw more than you have!", '#checking', true);
+            } else if (withdrawTotal === 0) {
+                msg("You're out of money!", '#checking', true);
+                checkBalance.push(debit);
+                output('WITHDRAW:', withdrawTotal)
+            } else {
+                checkBalance.push(debit);
+                output('WITHDRAW:', withdrawTotal)
+            }
+        }
+
+    });
+
 
 
 });
