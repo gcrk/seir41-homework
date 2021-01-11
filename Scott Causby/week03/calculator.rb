7ed5341d355ba95be7f29c78a3b4d86edb44b0bc
Scott Causby <scottcausby@gmail.com>
2021-01-11 21:11:47 +1100

    ruby calculator

---
+++ Scott Causby/week03/calculator.rb
@@ -0,0 +1,66 @@
+def show_menu
+    puts "Calculator".center(75) 
+    puts "-=" * 40
+    puts "[a] - addition"
+    puts "[s] - subtraction"
+    puts "[m] - multiplication"
+    puts "[d] - devision"
+    puts "[q] - Quit"
+    print "Enter your choice: "
+end
+
+show_menu
+menu_choice = gets.chomp.downcase
+
+until menu_choice == 'q'
+    case menu_choice
+    when 'a'
+        puts "Your first number to add"
+        num1 = gets.chomp
+        puts "Your second number to add"
+        num2 = gets.chomp
+
+        print "Your answer is: "
+        puts num1.to_i + num2.to_i
+    end
+
+    elsif 's'
+        puts "Your first number to subtract"
+        num1 = gets.chomp
+        puts "Your second number to subtract"
+        num2 = gets.chomp
+      
+        print "Your answer is: "
+        puts num1.to_i - num2.to_i
+    end 
+    
+
+    elsif 'm'
+        puts "Your first number to multiply"
+        num1 = gets.chomp
+        puts "Your second number to multiply"
+        num2 = gets.chomp
+  
+    print "Your answer is: "
+    puts num1.to_i * num2.to_i
+    end
+    
+
+    elsif 'd'
+        puts "Your first number to divide"
+        num1 = gets.chomp
+        puts "Your second number to divide"
+        num2 = gets.chomp
+      
+        print "Your answer is: "
+        puts num1.to_i / num2.to_i
+    end
+
+    else
+        puts 'Invalid selection.'
+
+    show_menu
+    menu_choice = gets.chomp.downcase
+    end
+
+puts "Thanks for using this amazing calculator!"
