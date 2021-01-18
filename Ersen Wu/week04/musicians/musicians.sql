55db9dc488a6f58fd996c7ecec35a52d0bbde0ea
Ersen <wuersen@hotmail.com>
2021-01-19 10:23:16 +1100

    wk4d2

---
+++ Ersen Wu/week04/musicians/musicians.sql
@@ -0,0 +1,9 @@
+CREATE TABLE musicians (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  genre TEXT,
+  image TEXT
+);
+
+INSERT INTO musicians (name, genre) VALUES ('Amy Winehouse', 'Jazz');
+INSERT INTO musicians (name, genre) VALUES ('Britney Spears', 'Pop');
