b38f0e7264ba73b99b904b1608962f185f4c20b6
Jye Harry <jyeharry21@hotmail.com>
2021-01-19 23:30:25 +1100

    rails crud app done

---
+++ Jye Harry/5-week/mountains_app/db/seeds.rb
@@ -0,0 +1,5 @@
+Mountain.destroy_all
+
+Mountain.create :name => 'Everest', :height => 8849, :country => 'Nepal, China'
+Mountain.create :name => 'Kosciuszko', :height => 2228, :country => 'Australia'
+Mountain.create :name => 'Fuji', :height => 3776, :country => 'Japan'
