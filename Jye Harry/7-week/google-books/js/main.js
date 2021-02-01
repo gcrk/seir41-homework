afdbe03aa493ae50facb6ac9bbd2e2d6ab02c84c
Jye Harry <jyeharry21@hotmail.com>
2021-02-01 19:24:04 +1100

    google books

---
+++ Jye Harry/7-week/google-books/js/main.js
@@ -0,0 +1,24 @@
+
+
+const getBookInfo = function() {
+  const xhr = new XMLHttpRequest();
+  const title = document.getElementById('title').value;
+  xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=${title}`);
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) return;
+    const data = JSON.parse(xhr.responseText);
+    const newData = data.items[0].volumeInfo;
+    document.querySelector('img').src = newData.imageLinks.thumbnail;
+    document.getElementById('title').innerHTML = newData.title;
+    document.getElementById('author').innerHTML = (newData.authors).join(', ');
+    document.getElementById('publisher').innerHTML = newData.publisher;
+    document.getElementById('description').innerHTML = newData.description;
+    document.querySelector('h4').hidden = false;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.getElementById('button').addEventListener('click', getBookInfo);
+});
