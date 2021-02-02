7d0a4145073327a714011fa1e8b7bfe8a2f25a59
Mai Nguyen <chimai.dk@gmail.com>
2021-02-02 22:44:42 +1100

    API-joke-and-hero without css

---
+++ Mai (Chi) Nguyen/week-07/day-02-api-web-app/js/joke.js
@@ -0,0 +1,21 @@
+const fetchJoke = function (event) {
+  event.preventDefault();
+
+  const type = $('input[name="joke-type"]:checked').val();
+  console.log(type);
+  const url = `https://official-joke-api.appspot.com/jokes/${type}/random`;
+
+  if (type) {
+    $.ajax(url).done(function(result) {
+      const content = result[0].setup;
+      $('#joke-content').html(`Content: ${content}`);
+
+      const punchline = result[0].punchline
+      $('#punchline').html(`Punchline: ${punchline}`);
+    })
+  }
+}
+
+$(document).ready(function () {
+  $('#joke-form').on('submit', fetchJoke);
+});
