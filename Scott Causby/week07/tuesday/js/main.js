94a29c1545d3fc6dac712db5b074c5a97ff8e127
Scott Causby <scottcausby@gmail.com>
2021-02-03 10:27:48 +1100

    tuesday homework update

--- Scott Causby/week07/tuesday/js/main.js
+++ Scott Causby/week07/tuesday/js/main.js
@@ -1,6 +1,6 @@
 const showAstronauts = function () {
     $.ajax('http://api.open-notify.org/astros.json').done(function (info) {
-      //$('<p>').text(info.people[0].name).appendTo('body');  
+      //$('<p>').text(info.people[0].name).appendTo('body'); previous 
       const people = info.people;
       for (let {name:n} of people) {
           $('<p>').text(n).appendTo('body');
