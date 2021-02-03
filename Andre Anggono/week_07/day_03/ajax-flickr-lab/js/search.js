0c07b9fd6723ce93ca368c8fb3bcec5b2e8b3e36
Andre Anggono <andre.anggono@hotmail.com>
2021-02-04 00:22:28 +1100

    Completed AJAX Flickr Lab

---
+++ Andre Anggono/week_07/day_03/ajax-flickr-lab/js/search.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+	$("#search").on("submit", function (e) {
+		e.preventDefault();
+		currentPage = 1;
+		const searchTerms = $("#query").val();
+		$("#images").empty();
+		searchFlickr(searchTerms, currentPage);
+	});
+
+	$(window).on("scroll", function () {
+		const scrollBottom = $(document).height() - $(document).scrollTop() - $(window).height();
+		if (scrollBottom < 700 && currentPage < totalPages) {
+			throttled();
+		}
+	});
+});
+
+let totalPages, currentPage;
+
+const searchFlickr = (keywords, page) => {
+	const flickrURL = "https://api.flickr.com/services/rest?jsoncallback=?"; // JSONP
+	$.getJSON(flickrURL, {
+		method: "flickr.photos.search",
+		api_key: "2f5ac274ecfac5a455f38745704ad084",
+		text: keywords,
+		format: "json",
+		page: page
+	})
+		.done(showImages)
+		.done((info) => {
+			totalPages = info.photos.pages;
+			console.log(info);
+			// console.log(info.photos.pages);
+		});
+};
+
+const showImages = (results) => {
+	_(results.photos.photo).each((photo) => {
+		const thumbnailURL = generateURL(photo);
+		const $img = $("<img></img>", { src: thumbnailURL, class: "col-4" }); // .attr('src', thumnailURL)
+		$img.appendTo("#images"); // $('#images').append($img)
+	});
+};
+
+const generateURL = (p) => ["http://farm", p.farm, ".static.flickr.com/", p.server, "/", p.id, "_", p.secret, "_q.jpg"].join("");
+
+const infiniteScroll = () => {
+	const searchTerms = $("#query").val();
+	currentPage++;
+	searchFlickr(searchTerms, currentPage);
+};
+
+const throttled = _(infiniteScroll).throttle(500, { trailing: false });
