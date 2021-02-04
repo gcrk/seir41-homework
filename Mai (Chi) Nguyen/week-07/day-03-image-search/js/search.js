518f5c54ba224d48cf3ccce3fb4cffcf03fc3c5c
Mai Nguyen <chimai.dk@gmail.com>
2021-02-05 03:42:52 +1100

    HW day 4 - Recursive functions finished -aaaaaaaaaa i need a new brain

--- Mai (Chi) Nguyen/week-07/day-03-image-search/js/search.js
+++ Mai (Chi) Nguyen/week-07/day-03-image-search/js/search.js
@@ -1,24 +1,34 @@
-const searchFlickr = function (keywords, page) {
+const state = {
+  nextPage: 1,
+  lastPageReached: false,
+}
+
+const searchFlickr = function (keywords) {
   // console.log('Searching for ', keywords);
+  if (state.lastPageReached) {
+    return;
+  }
+
   const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?'; // JSONP
   $.getJSON(flickrURL, {
     method: 'flickr.photos.search', // not to be confused with HTTP methods like GET/POST
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: keywords,
     format: 'json',    //this to tell flickr
-    page: page,
+    page: state.nextPage++,
     per_page: 20,
   }).done(showImages).done(function(info) {
+    if (info.photos.page >= info.photos.pages) {
+      state.lastPageReached = true;
+    }
     console.log(info);
+    console.log(state);
   });
 }
-// pages as global variable to track
-let currentPage = 1;
-let pages = 0;
 
 const showImages = function (results) {
   // console.log(results.photos.page);
-  pages = results.photos.pages;
+  state.totalPages = results.photos.pages;
   images = results.photos.photo;
   _(images).each(function(photo) {
     // generate a URL
@@ -51,8 +61,9 @@ const generatePhotoURL = function (p) {
 
 //Function to reset the page
 const resetSearch = function(newKeywords) {
-  currentPage = 1;
-  $('#images').html('');
+  state.nextPage = 1;
+  state.lastPageReached = false;
+  $('#images').empty();
 }
 
 $(document).ready(function(event) {
@@ -62,29 +73,25 @@ $(document).ready(function(event) {
     const searchTerms = $('#query').val();
     // reset the page with new search term
     resetSearch(searchTerms);
-    searchFlickr(searchTerms, currentPage);
+    searchFlickr(searchTerms);
 
   })
 
+  const chillSearchFlickr = _.debounce(searchFlickr, 3000, true);
+
     // Very twitchy
-  $(window).on('scroll', _.throttle(function () {
+  $(window).on('scroll', function () {
 
     //calculate the scrollbottom
     const scrollBottom = $(document).height() - $(document).scrollTop() - $(window).height();
     // console.log(scrollBottom);
     // if the scrollbottom is beneath some threshold
     if (scrollBottom < 500) {
-      console.log('near the bottom');
-      if (currentPage < pages) {
+      // console.log('near the bottom');
         const searchTerms = $('#query').val();
-        currentPage += 1;
-        searchFlickr(searchTerms, currentPage); // getting page 1 again ##TODO: HW
-      }
-      // if have 1 page, stop asking
-      // get rid of the photos of last search
+        chillSearchFlickr(searchTerms); // getting page 1 again ##TODO: HW
     }
-      // search Flickr again
-  }, 500))
+  })
 })
 
 
