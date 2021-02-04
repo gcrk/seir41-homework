2586f3ad0f3c3e5521992d5233f1f95646cd4332
Ersen <wuersen@hotmail.com>
2021-02-05 10:28:27 +1100

    fri

---
+++ Ersen Wu/week07/Thursday/recursion-exercises/recursion.js
@@ -0,0 +1,71 @@
+// Write code inside the functions
+// You will have to figure out what parameters to include
+// All functions must use recursion
+
+function findMax(arr){
+    // This function returns the largest number in a given array.
+    let index = 0;
+    let maxNum = arr[index];
+
+    function rMax(){
+      if(index === arr.length){
+        return maxNum
+      }
+
+      if(arr[index]>arr[index-1]){
+        maxNum = arr[index];
+        return rMax();
+      } else {maxNum = arr[index + 1]
+              return rMax();}
+     }
+        return rMax();
+}
+
+function factorial(n){
+    // This function returns the factorial of a given number.
+    if (n === 1){
+      return 1 ;
+    }
+
+     return n * factorial(n-1);
+}
+
+function fibonacci(n){
+    // This function returns the Nth number in the fibonacci sequence.
+    // https://en.wikipedia.org/wiki/Fibonacci_number
+    // For this function, the first two fibonacci numbers are 1 and 1
+    if(n===1||n===2){
+      return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+function coinFlips(n){
+    // This function returns an array of all possible outcomes from flipping a coin N times.
+    // Input type: Integer
+    // For example, coinFlips(2) would return the following:
+    // ["HH", "HT", "TH", "TT"]
+    // H stands for Heads and T stands for tails
+    // Represent the two outcomes of each flip as "H" or "T"
+    if(n===1){
+      return ['H','T']
+    }
+      return coinFlips(n-1).map(function(e){return e + 'H'}).concat(coinFlips(n-1).map(function(e){return e + 'T'}));
+}
+
+function letterCombinations(){
+    // This function returns an array of all combinations of the given letters
+    // Input type: Array of single characters
+    // For example, letterCombinations(["a","b","c"]) would return the following:
+    // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
+
+
+}
+
+module.exports = {
+    findMax,
+    factorial,
+    fibonacci,
+    coinFlips,
+    letterCombinations
+}
