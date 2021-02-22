7e3c0d19d8b53667f48f7c3a8e655f96902b1763
aronweston <aron@firststepdigital.com.au>
2021-02-22 23:02:43 +1100

    week 8 weather app and week 10 stonks + public holiday API fun

--- Aron Weston/week_8/weather_app/src/components/CurrentWeather.js
+++ Aron Weston/week_8/weather_app/src/components/CurrentWeather.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 const CurrentWeather = (props) => {
 
-    console.log(props);
+  if (props?.weather ?) {
     return (
-        <div className="output">
-            {props.weather.name}
-            {props.weather.main.humidity}
-            {props.weather.main.feels_like}
-            {props.weather.main.temp_max}
-            {props.weather.main.temp_min}
-            {props.weather.sys.sunrise}
-            {props.weather.sys.sunset}        
-        </div>
-    ); 
-}
+    <div className='output'>
+      {props?.weather?.name}
+      {props?.weather?.main.humidity}
+      {props?.weather?.main.feels_like}
+      {props?.weather?.main.temp_max}
+      {props?.weather?.main.temp_min}
+      {props?.weather?.sys.sunrise}
+      {props?.weather?.sys.sunset}
+    </div>
+  );
+  }
+  
+};
 
 export default CurrentWeather;
-
-
--- Aron Weston/week_8/weather_app/src/components/Weather.js
+++ Aron Weston/week_8/weather_app/src/components/Weather.js
@@ -1,83 +1,77 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import _ from 'underscore'
-import GetWeather from './GetWeather'
-import CurrentWeather from './CurrentWeather'
-import SearchWeather from './SearchWeather'
+import React, { Component } from 'react';
+import axios from 'axios';
+import _ from 'underscore';
+import GetWeather from './GetWeather';
+import CurrentWeather from './CurrentWeather';
+import SearchWeather from './SearchWeather';
 
 export class Weather extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            coords: [],
-            weather: [],
-            isLoaded: false
-        }
-        this.key = "da6cc8d124882796f1f64ad51e846e3c";
-        this.exclude = 'current,minutely,hourly,alerts'
-        this.getPosition = this.getPosition.bind(this);
-        this.getWeather = this.getWeather.bind(this);
-    }
-    
-    getPosition () {
-        if (!navigator.geolocation) {
-            alert("Geolocation is not supported by your browser");
-        } else {
-            navigator.geolocation.getCurrentPosition((pos) => {
-                this.setState({ coords: pos })
-                this.getWeather(this.state.coords)
-            }, (err) => {
-                console.error(`ERROR(${err.code}): ${err. message}`);
-            });
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      coords: [],
+      weather: [],
+    };
+    this.key = 'da6cc8d124882796f1f64ad51e846e3c';
+    this.exclude = 'current,minutely,hourly,alerts';
+    this.getPosition = this.getPosition.bind(this);
+    this.getWeather = this.getWeather.bind(this);
+  }
 
-    
-    async getWeather(input) {
-        
-        if (input.coords) {
-            try {
-                const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${input.coords.latitude}&lon=${input.coords.longitude}&appid=${this.key}&units=metric`)
-                const weather = res.data;
-                if (res.data) {
-                    this.setState({
-                        isLoaded: true,
-                        weather: weather
-                    })
-                }
-                console.log(this.state.weather);
-            } catch (error) {  
-               console.error(error)
-            }
-        } else {
-            try {
-                const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${this.key}&units=metric`)
-                // const weather = res.data;
-                // if (res.data) {
-                //     this.setState({
-                //         isLoaded: true,
-                //         weather: weather
-                //     })
-                // }
-            } catch (error) {
-                console.error(error)
-            }
+  getPosition() {
+    if (!navigator.geolocation) {
+      alert('Geolocation is not supported by your browser');
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.setState({ coords: pos });
+          this.getWeather(this.state.coords);
+        },
+        (err) => {
+          console.error(`ERROR(${err.code}): ${err.message}`);
         }
+      );
     }
-    
-    render() {
-        return (
-            <div>
-                <h1>Super Fun</h1>
-                <GetWeather onClick={this.getPosition} />
-                <SearchWeather onSubmit={this.getWeather} />
-                {this.state.isLoaded && 
-                    <CurrentWeather weather={this.state.weather} />
-                }
-            </div>
-        )
-      
+  }
+
+  async getWeather(input) {
+    if (input.coords) {
+      try {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${input.coords.latitude}&lon=${input.coords.longitude}&appid=${this.key}&units=metric`
+        );
+        const weather = res.data;
+        if (res.data) {
+          this.setState({
+            isLoaded: true,
+            weather: weather,
+          });
+        }
+        console.log(this.state.weather);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        const weather = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${this.key}&units=metric`
+        );
+      } catch (error) {
+        console.error(error);
+      }
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Super Fun</h1>
+        <GetWeather onClick={this.getPosition} />
+        <SearchWeather onSubmit={this.getWeather} />
+        <CurrentWeather weather={this.state.weather} />
+      </div>
+    );
+  }
 }
 
-export default Weather
+export default Weather;
