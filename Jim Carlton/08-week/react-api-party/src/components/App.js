186938a4b49e0b4447d3962e09660d6135db93dd
oisa <imucup@gmail.com>
2021-02-23 10:28:46 +1100

    W10 Sorting homework + initial stocks homework on Node.

--- Jim Carlton/08-week/react-api-party/src/components/App.js
+++ Jim Carlton/08-week/react-api-party/src/components/App.js
@@ -7,6 +7,15 @@ import { Helmet } from "react-helmet";
 function App() {
   return (
     <div className="App">
+
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <meta http-equiv='cache-control' content='no-cache' />
+        <meta http-equiv='expires' content='0' />
+        <meta http-equiv='pragma' content='no-cache' />
+      </Helmet>
+
       <nav><h1>React App Party</h1></nav>
       <YourLocation />
       <RandomQuote />
